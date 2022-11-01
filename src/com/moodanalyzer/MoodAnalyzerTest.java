283ef43eed20142d8145990f6e330bb8f663ae2f
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-11-01 16:31:06 +0530

    [Dipali] Added : exception handling for invalid mood

--- src/com/moodanalyzer/MoodAnalyzerTest.java
+++ src/com/moodanalyzer/MoodAnalyzerTest.java
@@ -1,22 +1,54 @@
 package com.moodanalyzer;
 
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import junit.framework.Assert;
 
 public class MoodAnalyzerTest {
-
 	@Test
-	public void testMoodAnalyse() {
+	public void testMoodAnalyse() throws MoodAnalyzer, MoodAnalyzerException {
 		MoodAnalyzer md = new MoodAnalyzer();
 		String mood = md.analyseMood("This is a SAD message");
 		Assert.assertEquals("SAD", mood);
 	}
 
 	@Test
-	public void testHappyMood() {
+	public void testHappyMood() throws MoodAnalyzer, MoodAnalyzerException {
 		MoodAnalyzer md = new MoodAnalyzer();
 		String mood = md.analyseMood("This is a HAPPY message");
 		Assert.assertEquals("HAPPY", mood);
+	}
+
+	@Test
+	public void givenNullMoodShouldReturnHappy() throws MoodAnalyzer, MoodAnalyzerException {
+		MoodAnalyzer md = new MoodAnalyzer(null);
+		String mood = md.analyseMood("This is a HAPPY message");
+		Assert.assertEquals("HAPPY", mood);
+	}
+
+	@Test
+	public void givenInAnyMood_Should_Return_Happy() throws MoodAnalyzer, MoodAnalyzerException {
+		MoodAnalyzer md = new MoodAnalyzer();
+		String mood = md.analyseMood("This is in any mood");
+		Assert.assertEquals("HAPPY", mood);
+	}
+
+	@Test
+	public void givenInHappy_MoodShould_ReturnSad() throws MoodAnalyzer, MoodAnalyzerException {
+		MoodAnalyzer md = new MoodAnalyzer();
+		String mood = md.analyseMood("This is in happy mood");
+		Assert.assertEquals("HAPPY", mood);
+	}
+
+	@Test
+	public void givenNullMoodShouldThrowException() throws MoodAnalyzer {
+		MoodAnalyzer md = new MoodAnalyzer(null);
+		try {
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(MoodAnalyzerException.class);
+			md.analyseMood(null);
+		} catch (Exception e) {
+		}
 	}
 }
